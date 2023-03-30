be918e08d93b915c18d852a377501e0b0bdf1f6e
twcodecuv <timothy.wiggan@clinuvel.com>
2023-03-30 17:01:03 +0100

    This is the new header

--- header.php
+++ header.php
@@ -20,6 +20,29 @@
 
 
     <style>
+    a {
+        display: block;
+        width: 185px;
+        padding: 1em;
+    }
+
+    /* This is for Id specific sub-menu styles */
+
+    li>.sub-menu {
+        left: 0;
+        border-radius: 10px;
+    }
+
+
+
+
+    .sub-menu .sub-menu {
+        padding: 0;
+        left: 200px;
+        top: 0;
+    }
+
+    /* This is for Id specific sub-menu styles */
     .nav_container {
         width: 100%;
         background: #000;
@@ -31,7 +54,21 @@
         display: flex;
         justify-content: space-between;
         list-style: none;
+        padding: 0 15em;
     }
+
+    .sub-menu {
+        margin: 0;
+        padding: 0;
+        background: red;
+    }
+
+    .sub-menu>li {
+        padding-top: 1em;
+        width: 200px;
+
+    }
+
 
     li>a {
         color: #fff;
@@ -39,11 +76,40 @@
         text-decoration: none;
     }
 
-    .sub-menu {
-
-        display: none;
-
+    li {
+        position: relative;
+        display: block;
+        transition-duration: 0.5s;
     }
+
+    .sub-menu>li:hover {
+        color: blue;
+        cursor: pointer;
+        background: blue;
+    }
+
+    ul li ul {
+        visibility: hidden;
+        opacity: 0;
+        position: absolute;
+        transition: all 0.5s ease;
+        margin-top: 1em;
+        left: 30%;
+        display: none;
+    }
+
+
+    ul li:hover>ul,
+    ul li:focus-within>ul,
+    ul li ul:hover {
+
+        visibility: visible;
+        opacity: 1;
+        display: block;
+    }
+
+
+
 
     body {
         margin: 0;
